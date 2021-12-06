daf71b918697b8bd62cb0897c15cbb652a02bba1
GEMCorp <modestus58@gmail.com>
2021-12-06 22:32:14 +0000

    First vectorized approach to computeCost.m

--- _ex1-octave/computeCost.m
+++ _ex1-octave/computeCost.m
@@ -5,17 +5,18 @@ function J = computeCost(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
-% You need to return the following variables correctly 
-J = 0;
-
+% You need to return the following variables correctly
+modelPredictions = zeros(1,m);
+for i = 0:m,
+   modelPredictions(i) = theta' * X(i);
+end
+sqrdErrors = zeros(1,m);
+sqrdErrors = (modelPredictions' - y).^ 2;
+J = 0.0;
+J = (1/(2m)) * sum(sqrdErrors);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
-
 
 % =========================================================================
 
