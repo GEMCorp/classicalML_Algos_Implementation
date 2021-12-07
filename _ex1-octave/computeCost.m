f77266215a4bb6fc2705bce7f4675e1e677a3d8b
GEMCorp <modestus58@gmail.com>
2021-12-07 10:46:22 +0000

    Fixed errors in computeCost

--- _ex1-octave/computeCost.m
+++ _ex1-octave/computeCost.m
@@ -5,15 +5,17 @@ function J = computeCost(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-% You need to return the following variables correctly
-modelPredictions = zeros(1,m);
-for i = 0:m,
-   modelPredictions(i) = theta' * X(i);
-end
-sqrdErrors = zeros(1,m);
-sqrdErrors = (modelPredictions' - y).^ 2;
+%You need to return the following variables correctly
+##modelPredictions = zeros(1,m);
+##for i = 0:m,
+##   modelPredictions(i) = theta' * X(i);
+##end
+modelPredictions = X * theta;
+##sqrdErrors = zeros(1,m);
+sqrdErrors = (modelPredictions - y).^ 2;
+summation = sum(sqrdErrors);
 J = 0.0;
-J = (1/(2m)) * sum(sqrdErrors);
+J = summation/(2*m);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
