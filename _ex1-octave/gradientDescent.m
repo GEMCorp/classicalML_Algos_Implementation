ef66a42651f2ae45bd18bab1ba2eecedddf9d158
GEMCorp <modestus58@gmail.com>
2021-12-10 14:11:27 +0000

    Completed gradient descent for linear regression

--- _ex1-octave/gradientDescent.m
+++ _ex1-octave/gradientDescent.m
@@ -16,15 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    c = alpha/m;
+    h = X * theta;
+    deltaJ = X' *(h - y);
+    theta -= c * deltaJ; 
 
-
-
-
-
-
-
-    % ============================================================
-
+    % ===========================================================
+ 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
 
