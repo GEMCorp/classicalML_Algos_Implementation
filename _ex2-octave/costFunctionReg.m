9b3958bd76b31ed8f5ae6d18901e457d65b9c847
GEMCorp <modestus58@gmail.com>
2021-12-14 18:36:12 +0000

    Completed regularized logistic regression but not submitted

--- _ex2-octave/costFunctionReg.m
+++ _ex2-octave/costFunctionReg.m
@@ -17,11 +17,16 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+c = 1/m;
+posClass = (-y)'*log(h);
+negClass = (-1+y)'*log(1-h);
+thetaReg = theta([2:size(theta,1)],:);
+J = c*((posClass + negClass)+ (lambda/2)*sum(thetaReg.^2));
 
-
-
-
-
+err = h - y;
+grad += c*(((X')*err)+(lambda*theta));
+grad(1) -= (lambda*theta(1))/m;
 % =============================================================
 
 end
