c4a9431f33eeb0eccab0a67437419f9ae13c1d6b
GEMCorp <modestus58@gmail.com>
2021-12-20 02:26:39 +0000

    PredictOneVsAll completed,retVal of fmincg not understood

--- _ex3-octave/predictOneVsAll.m
+++ _ex3-octave/predictOneVsAll.m
@@ -29,8 +29,9 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-
-
+z = X*all_theta';%review:fmincg caused dimension compatiblity musunderstandings
+A = sigmoid(z);
+[max_values, p] = max(A, [], 2);
 
 
 
